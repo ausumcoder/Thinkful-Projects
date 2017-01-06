628af890aed97ec9b115854beaaac85bcddbaf2f
ausumcoder <ramya.roberthalf@gmail.com>
2017-01-06 10:27:04 -0800

    Second Commit

--- Web Project1/script.js
+++ Web Project1/script.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 app.directive('yoYo',function(){
 
 	return{
-		template: '<strong> Heyy Yo! </strong>',
+		template: '<strong> Heyy Yo Yo! </strong>',
 		restrict: 'E'
 	}
 })
