e4531fa6f3b56dba298c66d9891ce13bd3df26fb
ausomecoder <ramya.roberthalf@gmail.com>
2017-02-01 07:31:22 -0800

    new commit with bower

---
+++ ngMadlibs/js/script.js
@@ -0,0 +1,8 @@
+angular.module('myApp',[])
+.controller('ngmadLibs', function($scope) {
+
+  $scope.gender = 'male';
+  $scope.female_name = "girlname";
+  $scope.male_name = "boyname";
+  $scope.job_title = "masonry"; 
+});
